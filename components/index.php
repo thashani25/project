bb54e2d919e1d5349bc8bead292325065bd8eddf
Piyunil Thashani <piyunilthashani4@gmail.com>
2025-02-26 20:58:52 +0530

    int

---
+++ components/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dentist website </titel>
+
+    <!---fonts-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+    <!--- custom css fill-->
+    <link rel="stylesheet" href="components/style.css">
+
+
+
+</head>
+<body>
+
+
+
+<!---js fill--->
+<script src="js/script.js"></script>
+    
+</body>
+</html>
